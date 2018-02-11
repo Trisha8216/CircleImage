207fd3f158a529dfd97a92ecc9165cb5dee55453
Trisha Das <trisha.tyc0@gmail.com>
2018-02-10 16:10:07 -0800

    Initial commit

---
+++ app/src/main/java/com/trisha/circleimage/MainActivity.java
@@ -0,0 +1,29 @@
+package com.trisha.circleimage;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image=(ImageView)findViewById(R.id.imageView);
+
+        Bitmap bitmap= BitmapFactory.decodeResource(getResources(),R.drawable.image);
+        RoundedBitmapDrawable roundedBitmapDrawable= RoundedBitmapDrawableFactory.create(getResources(),bitmap);
+        roundedBitmapDrawable.setCircular(true);
+        image.setImageDrawable(roundedBitmapDrawable);
+
+
+
+    }
+}
